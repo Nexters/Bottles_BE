b933dc1d801440a6b10146c3bff45016f9397c76
injoon2019 <ison@sfu.ca>
2024-07-14 23:44:41 +0900

    init

---
+++ src/main/kotlin/com/nexters/bottles/BottlesApplication.kt
@@ -0,0 +1,11 @@
+package com.nexters.bottles
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BottlesApplication
+
+fun main(args: Array<String>) {
+	runApplication<BottlesApplication>(*args)
+}
