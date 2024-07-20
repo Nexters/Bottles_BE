d580aaf872eaa2a0a72790e178fbcf43fc6e10ad
injoon2019 <ison@sfu.ca>
2024-07-21 02:31:11 +0900

    feat: api v1 설정

--- src/main/kotlin/com/nexters/bottles/profile/controller/ProfileController.kt
+++ src/main/kotlin/com/nexters/bottles/profile/controller/ProfileController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 class ProfileController(
     private val profileFacade: ProfileFacade,
 ) {
