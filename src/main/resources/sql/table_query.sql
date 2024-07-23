b385fc13446693d1d3e593792f1258851da3fe51
Miseong Kim <miseongk@naver.com>
2024-07-23 13:39:39 +0900

    refactor: 테이블 쿼리 TIMESTAMP를 DATETIME으로 변경

--- src/main/resources/sql/table_query.sql
+++ src/main/resources/sql/table_query.sql
@@ -5,26 +5,26 @@ CREATE TABLE User
     kakao_id     VARCHAR(255) DEFAULT NULL,
     phone_number VARCHAR(255) DEFAULT NULL,
     gender       VARCHAR(10)  DEFAULT 'MALE',
-    created_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
-    updated_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    created_at   DATETIME     DEFAULT CURRENT_TIMESTAMP,
+    updated_at   DATETIME     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE user_profile
 (
     id             BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_id        BIGINT                              NOT NULL,
+    user_id        BIGINT                             NOT NULL,
     profile_select JSON,
     introduction   JSON,
-    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
+    created_at     DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at     DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE bottle
 (
     id             BIGINT AUTO_INCREMENT PRIMARY KEY,
-    target_user_id BIGINT                              NOT NULL,
-    source_user_id BIGINT                              NOT NULL,
-    expired_at     TIMESTAMP                           NOT NULL,
-    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
+    target_user_id BIGINT                             NOT NULL,
+    source_user_id BIGINT                             NOT NULL,
+    expired_at     DATETIME                           NOT NULL,
+    created_at     DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at     DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
 )
