2a350196c275cadd8192ee534de39994944ea30a
Miseong Kim <miseongk@naver.com>
2024-07-23 22:29:31 +0900

    chore: 테이블 추가

--- src/main/resources/sql/table_query.sql
+++ src/main/resources/sql/table_query.sql
@@ -28,3 +28,32 @@ CREATE TABLE bottle
     created_at     DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at     DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
 )
+
+CREATE TABLE ping_pong
+(
+    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_a_id       BIGINT                                NOT NULL,
+    user_b_id       BIGINT                                NOT NULL,
+    stopped_user_id BIGINT,
+    status          VARCHAR(20) DEFAULT 'ACTIVE'          NOT NULL,
+    created_at      DATETIME    DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at      DATETIME    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
+)
+
+CREATE TABLE letter
+(
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    ping_pong_id BIGINT                             NOT NULL,
+    user_id      BIGINT                             NOT NULL,
+    letters      JSON                               NOT NULL,
+    image        TEXT,
+    is_read      BOOLEAN  DEFAULT FALSE             NOT NULL,
+    created_at   DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at   DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
+)
+
+CREATE TABLE question
+(
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    question VARCHAR(255) NOT NULL
+)
