3abfbc0a888e841573f851befcdaad7b3d3bb340
인준 <ison@sfu.ca>
2024-07-24 19:09:47 +0900

    docs: swagger에서 확인할 수 있는 문구 추가 (#38)

--- src/main/kotlin/com/nexters/bottles/bottle/controller/BottleController.kt
+++ src/main/kotlin/com/nexters/bottles/bottle/controller/BottleController.kt
@@ -3,6 +3,7 @@ package com.nexters.bottles.bottle.controller
 import com.nexters.bottles.bottle.facade.BottleFacade
 import com.nexters.bottles.bottle.facade.dto.BottleDetailResponseDto
 import com.nexters.bottles.bottle.facade.dto.BottleListResponseDto
+import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -15,16 +16,19 @@ class BottleController(
     private val bottleFacade: BottleFacade
 ) {
 
+    @ApiOperation("홈 - 받은 보틀 목록 조회하기")
     @GetMapping
     fun getBottlesList(): BottleListResponseDto {
         return bottleFacade.getBottles()
     }
 
+    @ApiOperation("홈 - 보틀 상세 정보 조회하기")
     @GetMapping("/{bottleId}")
     fun getBottleDetail(@PathVariable bottleId: Long): BottleDetailResponseDto {
         return bottleFacade.getBottle(bottleId)
     }
 
+    @ApiOperation("홈 - 보틀에 내 소개 보내기(수락하기)")
     @PostMapping("/{bottleId}/accept")
     fun acceptBottle(@PathVariable bottleId: Long) {
         bottleFacade.acceptBottle(bottleId)
