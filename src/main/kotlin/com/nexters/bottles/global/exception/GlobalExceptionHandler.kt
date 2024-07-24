0f7a9a6a9ffce34b278eef08b1ff501bfc1b4674
Miseong Kim <miseongkinn@gmail.com>
2024-07-24 20:42:15 +0900

    [Feature/41] 전역적으로 예외 처리를 한다 (#42)

---
+++ src/main/kotlin/com/nexters/bottles/global/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,21 @@
+package com.nexters.bottles.global.exception
+
+import mu.KotlinLogging
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.servlet.http.HttpServletRequest
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    private val log = KotlinLogging.logger { }
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    fun handle(request: HttpServletRequest, e: IllegalArgumentException): ErrorResponseDto {
+        log.warn { "Error occured at path: ${request.requestURI}, message: ${e.message}" }
+        return ErrorResponseDto(e.message)
+    }
+}
