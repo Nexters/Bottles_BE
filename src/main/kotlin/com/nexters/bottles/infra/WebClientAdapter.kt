5ebaf0f0ee8a22aa156509db8e0c1a386f71b9a9
injoon2019 <ison@sfu.ca>
2024-07-27 16:31:39 +0900

    feat: sms 발송 요청 api를 구현한다

--- src/main/kotlin/com/nexters/bottles/infra/WebClientAdapter.kt
+++ src/main/kotlin/com/nexters/bottles/infra/WebClientAdapter.kt
@@ -3,10 +3,28 @@ package com.nexters.bottles.infra
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import com.nexters.bottles.auth.facade.dto.KakaoUserInfoResponse
+import com.nexters.bottles.auth.facade.dto.MessageDTO
+import com.nexters.bottles.auth.facade.dto.SmsRequestDTO
+import com.nexters.bottles.auth.facade.dto.SmsResponseDTO
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.BodyInserters
 
 @Component
 class WebClientAdapter(
     private val webClient: WebClient,
+
+    @Value("\${naver-cloud-sms.accessKey}")
+    val naverSmsAccessKey: String,
+
+    @Value("\${naver-cloud-sms.secretKey}")
+    val naverSmsSecretKey: String,
+
+    @Value("\${naver-cloud-sms.serviceId}")
+    val naverSmsServiceId: String,
+
+    @Value("\${naver-cloud-sms.senderPhone}")
+    val naverSmsSenderPhone: String,
 ) {
 
     fun sendAuthRequest(code: String): KakaoUserInfoResponse {
@@ -17,5 +35,41 @@ class WebClientAdapter(
             .retrieve()
             .bodyToMono(KakaoUserInfoResponse::class.java)
             .block() ?: throw IllegalArgumentException("회원가입에 대해 문의해주세요")
+    }
+
+    fun sendSms(time: Long, messageDto: MessageDTO, signature: String): SmsResponseDTO? {
+        val headers = mapOf(
+            "Content-Type" to MediaType.APPLICATION_JSON_VALUE,
+            "x-ncp-apigw-timestamp" to time.toString(),
+            "x-ncp-iam-access-key" to naverSmsAccessKey,
+            "x-ncp-apigw-signature-v2" to signature
+        )
+
+        val request = SmsRequestDTO(
+            type = "SMS",
+            contentType = "COMM",
+            countryCode = "82",
+            from = naverSmsSenderPhone,
+            content = messageDto.content,
+            messages = listOf(messageDto)
+        )
+
+        val webClient = WebClient.builder()
+            .baseUrl("https://sens.apigw.ntruss.com")
+            .defaultHeaders { httpHeaders ->
+                headers.forEach { (key, value) ->
+                    httpHeaders.add(key, value)
+                }
+            }
+            .build()
+
+        val response: SmsResponseDTO? = webClient.post()
+            .uri("/sms/v2/services/$naverSmsServiceId/messages")
+            .body(BodyInserters.fromValue(request))
+            .retrieve()
+            .bodyToMono(SmsResponseDTO::class.java)
+            .block()
+
+        return response
     }
 }
