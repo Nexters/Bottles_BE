77dcb96f761bd2110e8ffcc9861a0232e75ad013
injoon2019 <ison@sfu.ca>
2024-08-10 13:32:29 +0900

    feat: 유저 프로필 생성 여부도 로그인 시 알려준다

--- api/src/main/kotlin/com/nexters/bottles/api/auth/facade/dto/KakaoSignInUpResponse.kt
+++ api/src/main/kotlin/com/nexters/bottles/api/auth/facade/dto/KakaoSignInUpResponse.kt
@@ -4,5 +4,6 @@ data class KakaoSignInUpResponse(
     val accessToken: String,
     val refreshToken: String,
     val isSignUp: Boolean = false,
+    val hasCompleteUserProfile: Boolean = false,
     val hasCompleteIntroduction: Boolean = false,
 )
--- api/src/main/kotlin/com/nexters/bottles/api/auth/facade/dto/SmsSignInResponse.kt
+++ api/src/main/kotlin/com/nexters/bottles/api/auth/facade/dto/SmsSignInResponse.kt
@@ -3,6 +3,7 @@ package com.nexters.bottles.api.auth.facade.dto
 data class SmsSignInResponse(
     val accessToken: String,
     val refreshToken: String,
+    val hasCompleteUserProfile: Boolean = false,
     val hasCompleteIntroduction: Boolean = false,
 ) {
 }
