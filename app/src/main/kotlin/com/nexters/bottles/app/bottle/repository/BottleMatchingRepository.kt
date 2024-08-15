e22bbc1be77f89eded49749a26931d9feb01d84b
인준 <ison@sfu.ca>
2024-08-15 21:06:52 +0900

    feat: 이성과 매칭 & 자기소개 작성 안한 유저와 매칭하지 않기 (#207)

--- app/src/main/kotlin/com/nexters/bottles/app/bottle/repository/BottleMatchingRepository.kt
+++ app/src/main/kotlin/com/nexters/bottles/app/bottle/repository/BottleMatchingRepository.kt
@@ -1,6 +1,7 @@
 package com.nexters.bottles.app.bottle.repository
 
 import com.nexters.bottles.app.bottle.repository.dto.UsersCanBeMatchedDto
+import com.nexters.bottles.app.user.domain.enum.Gender
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
 
@@ -33,20 +34,33 @@ class BottleMatchingRepository(
     /*
     각 user의 bottle_history에 matched_user_id가 아닌 user 조회
      */
-    fun findAllUserCanBeMatched(userId: Long): List<UsersCanBeMatchedDto> {
+    // TODO 쿼리 개선. 로직이 너무 쿼리에 들어가는 것 같기도
+    fun findAllUserCanBeMatched(userId: Long, gender: Gender): List<UsersCanBeMatchedDto> {
         val sql = """
-           SELECT u.id AS willMatchUserId, u.gender AS willMatchUserGender, u.city AS city
-           FROM user u 
-           LEFT JOIN bottle_history bh ON u.id = bh.matched_user_id 
-           WHERE bh.matched_user_id IS NULL AND u.id != ? AND u.deleted = false AND u.is_match_activated = true;
-       """.trimIndent()
+       SELECT u.id AS willMatchUserId, u.gender AS willMatchUserGender, u.city AS city 
+       FROM user u 
+       JOIN user_profile up ON up.user_id = u.id 
+           AND up.image_url IS NOT NULL 
+           AND up.introduction IS NOT NULL 
+           AND JSON_LENGTH(up.introduction) > 0 
+       LEFT JOIN bottle_history bh ON u.id = bh.matched_user_id 
+       WHERE bh.matched_user_id IS NULL 
+         AND u.id != ? 
+         AND u.gender != ? 
+         AND u.deleted = false 
+         AND u.is_match_activated = true;
+    """.trimIndent()
 
-        return jdbcTemplate.query(sql, { rs, _ ->
-            UsersCanBeMatchedDto(
-                willMatchUserId = rs.getLong("willMatchUserId"),
-                willMatchUserGender = rs.getString("willMatchUserGender"),
-                willMatchCity = rs.getString("city"),
-            )
-        }, userId)
+        return jdbcTemplate.query(
+            sql,
+            arrayOf(userId, gender),
+            { rs, _ ->
+                UsersCanBeMatchedDto(
+                    willMatchUserId = rs.getLong("willMatchUserId"),
+                    willMatchUserGender = rs.getString("willMatchUserGender"),
+                    willMatchCity = rs.getString("city")
+                )
+            }
+        )
     }
 }
