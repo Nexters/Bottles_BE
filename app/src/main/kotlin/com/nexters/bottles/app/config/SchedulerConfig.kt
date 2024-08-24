c58a23116ff18a0ced999a4fe2059cc2eb0252f7
Miseong Kim <miseongkinn@gmail.com>
2024-08-24 15:34:31 +0900

    [Feature/331] 푸시 스케줄러 app으로 임시 이동한다 (#332)

---
+++ app/src/main/kotlin/com/nexters/bottles/app/config/SchedulerConfig.kt
@@ -0,0 +1,9 @@
+package com.nexters.bottles.app.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class SchedulerConfig {
+}
---
+++ app/src/main/kotlin/com/nexters/bottles/app/notification/component/FcmNotificationScheduler.kt
@@ -0,0 +1,28 @@
+package com.nexters.bottles.app.notification.component
+
+import com.nexters.bottles.app.notification.component.dto.FcmNotification
+import com.nexters.bottles.app.notification.service.FcmTokenService
+import com.nexters.bottles.app.user.service.UserService
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class FcmNotificationScheduler(
+    private val fcmClient: FcmClient,
+    private val fcmTokenService: FcmTokenService,
+    private val userService: UserService
+) {
+
+    @Scheduled(cron = "0 0 18 * * *")
+    fun notifyMatching() {
+        val users = userService.findAllByNotDeleted()
+        val fcmTokens = fcmTokenService.findByUsers(users)
+        val tokens = fcmTokens.map { it.token }
+
+        val fcmNotification = FcmNotification(
+            title = "새로운 보틀이 떠내려왔어요 🏖️",
+            body = "모래사장에 새로운 보틀이 도착했어요.\n도착한 보틀을 확인해주세요!"
+        )
+        fcmClient.sendNotificationAll(userTokens = tokens, fcmNotification = fcmNotification)
+    }
+}
