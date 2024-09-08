21ce7ca67e3f4c879bd873f4febe2cac91adac93
Miseong Kim <miseongkinn@gmail.com>
2024-09-08 15:25:48 +0900

    [Feature/356] ELK를 구축한다 (#388)
    * build: logstash logback encoder dependency 추가
    
    * chore: elk 구축
    
    * chore: elk deploy workflow 추가
    
    * chore: log를 Logstash로 전달하도록 설정
    
    * chore: docker 실행시키는 부분 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,19 +11,21 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     api("org.springframework.boot:spring-boot-starter-data-jdbc")
 
+    api("net.logstash.logback:logstash-logback-encoder:7.3")
+
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
 
     implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
 
     implementation("com.google.firebase:firebase-admin:9.2.0")
-
+    
     runtimeOnly("com.h2database:h2")
     runtimeOnly("mysql:mysql-connector-java")
 
     val isMacOS: Boolean = System.getProperty("os.name").startsWith("Mac OS X")
     val architecture = System.getProperty("os.arch").toLowerCase()
-    if (isMacOS && architecture == "aarch64")  {
+    if (isMacOS && architecture == "aarch64") {
         implementation("io.netty:netty-resolver-dns-native-macos:4.1.75.Final") {
             artifact {
                 classifier = "osx-aarch_64"
