702976234ed3f25457531e37868da28e02ad2967
injoon2019 <ison@sfu.ca>
2024-09-10 21:23:30 +0900

    feat: 주석 추가

--- app/src/main/kotlin/com/nexters/bottles/app/user/domain/enum/AlimyType.kt
+++ app/src/main/kotlin/com/nexters/bottles/app/user/domain/enum/AlimyType.kt
@@ -1,9 +1,9 @@
 package com.nexters.bottles.app.user.domain.enum
 
 enum class AlimyType {
-    DAILY_RANDOM,
-    RECEIVE_LIKE,
-    PINGPONG,
-    MARKETING,
+    DAILY_RANDOM, // 떠다니는 보틀 알림
+    RECEIVE_LIKE, // 호감 도착 알림
+    PINGPONG, // 대화 알림
+    MARKETING, // 마케팅 수신 동의
     ;
 }
