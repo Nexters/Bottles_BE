f625f00c824ebaa98f4e552ea7e362327c487429
injoon2019 <ison@sfu.ca>
2024-09-15 17:58:54 +0900

    feat: 유저 프로필 조회 API에 카카오 아이디 추가

--- api/src/main/kotlin/com/nexters/bottles/api/user/facade/dto/UserProfileResponse.kt
+++ api/src/main/kotlin/com/nexters/bottles/api/user/facade/dto/UserProfileResponse.kt
@@ -6,6 +6,7 @@ import com.nexters.bottles.app.user.domain.UserProfileSelect
 data class UserProfileResponse(
     val userName: String?,
     val age: Int,
+    val kakaoId: String?,
     val imageUrl: String? = null,
     val introduction: List<QuestionAndAnswer>,
     val profileSelect: UserProfileSelect? = null,
