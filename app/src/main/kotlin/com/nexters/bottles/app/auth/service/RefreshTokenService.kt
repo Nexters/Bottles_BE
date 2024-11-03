3cdb0247416e57da4338fcd2ed59f17f699a5081
injoon2019 <ison@sfu.ca>
2024-11-04 01:50:30 +0900

    feat: 리프레시 토큰 삭제시 예외 낮추기

--- app/src/main/kotlin/com/nexters/bottles/app/auth/service/RefreshTokenService.kt
+++ app/src/main/kotlin/com/nexters/bottles/app/auth/service/RefreshTokenService.kt
@@ -2,6 +2,8 @@ package com.nexters.bottles.app.auth.service
 
 import com.nexters.bottles.app.auth.domain.RefreshToken
 import com.nexters.bottles.app.auth.repository.RefreshTokenRepository
+import mu.KotlinLogging
+import org.springframework.orm.ObjectOptimisticLockingFailureException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
@@ -11,6 +13,8 @@ class RefreshTokenService(
     private val refreshTokenRepository: RefreshTokenRepository,
 ) {
 
+    private val log = KotlinLogging.logger {  }
+
     @Transactional
     fun delete(userId: Long) {
         refreshTokenRepository.findAllByUserId(userId)
@@ -19,15 +23,17 @@ class RefreshTokenService(
 
     @Transactional
     fun upsertRefreshToken(userId: Long, refreshToken: String, expiryDate: LocalDateTime) {
-        val refreshTokens = refreshTokenRepository.findAllByUserId(userId)
+        try {
+            val refreshTokens = refreshTokenRepository.findAllByUserId(userId)
 
-        if (refreshTokens.isNotEmpty()) {
-            refreshTokens.forEach {
-                refreshTokenRepository.deleteById(it.id)
+            if (refreshTokens.isNotEmpty()) {
+                refreshTokenRepository.deleteAllInBatch(refreshTokens)
+                saveRefreshToken(userId, refreshToken, expiryDate)
+            } else {
+                saveRefreshToken(userId, refreshToken, expiryDate)
             }
-            saveRefreshToken(userId, refreshToken, expiryDate)
-        } else {
-            saveRefreshToken(userId, refreshToken, expiryDate)
+        } catch (e: ObjectOptimisticLockingFailureException) {
+            log.warn { "리프레시 토큰 삭제에서 예외 발생"}
         }
     }
 
