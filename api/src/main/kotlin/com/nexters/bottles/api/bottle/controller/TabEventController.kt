db3f45d80afccc75842f927ff12291bd637f88af
Miseong Kim <miseongkinn@gmail.com>
2024-11-05 01:11:02 +0900

    feat: sse connect api endpoint 수정

--- api/src/main/kotlin/com/nexters/bottles/api/bottle/controller/TabEventController.kt
+++ api/src/main/kotlin/com/nexters/bottles/api/bottle/controller/TabEventController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/tab")
 class TabEventController(
     private val tabEventService: TabEventService
 ) {
