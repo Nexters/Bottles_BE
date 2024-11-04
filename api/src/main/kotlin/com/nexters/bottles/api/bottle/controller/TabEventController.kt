e947a7e6117f5ea6e655bde4f77ea3cbd1806f0f
Miseong Kim <miseongkinn@gmail.com>
2024-11-05 01:20:35 +0900

    [Feature/546] sse connect api endpoint 수정한다 (#547)
    * feat: sse connect api endpoint 수정
    
    * feat: sse connect api endpoint 수정

--- api/src/main/kotlin/com/nexters/bottles/api/bottle/controller/TabEventController.kt
+++ api/src/main/kotlin/com/nexters/bottles/api/bottle/controller/TabEventController.kt
@@ -5,15 +5,17 @@ import com.nexters.bottles.api.global.resolver.AuthUserId
 import com.nexters.bottles.app.bottle.service.TabEventService
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 
 @RestController
+@RequestMapping("/api/v1/tab")
 class TabEventController(
     private val tabEventService: TabEventService
 ) {
 
-    @GetMapping("/connect", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    @GetMapping("/connect/sse", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
     @AuthRequired
     fun connectSse(@AuthUserId userId: Long): SseEmitter {
         val sseEmitter = tabEventService.connectSse(userId)
