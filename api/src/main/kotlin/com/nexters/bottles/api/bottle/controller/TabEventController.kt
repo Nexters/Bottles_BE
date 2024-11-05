62a1ace63d7e3eb824010778654defd99bcdf1a6
Miseong Kim <miseongkinn@gmail.com>
2024-11-06 01:52:06 +0900

    feat: connect sse api 설명 추가

--- api/src/main/kotlin/com/nexters/bottles/api/bottle/controller/TabEventController.kt
+++ api/src/main/kotlin/com/nexters/bottles/api/bottle/controller/TabEventController.kt
@@ -3,6 +3,7 @@ package com.nexters.bottles.api.bottle.controller
 import com.nexters.bottles.api.global.interceptor.AuthRequired
 import com.nexters.bottles.api.global.resolver.AuthUserId
 import com.nexters.bottles.app.bottle.service.TabEventService
+import io.swagger.annotations.ApiOperation
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,6 +16,7 @@ class TabEventController(
     private val tabEventService: TabEventService
 ) {
 
+    @ApiOperation("탭바 SSE 연결 - New 뱃지 여부 이벤트")
     @GetMapping("/connect/sse", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
     @AuthRequired
     fun connectSse(@AuthUserId userId: Long): SseEmitter {
