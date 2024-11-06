703f63773376c22f1573088b7fb9ca8ed421ae96
injoon2019 <ison@sfu.ca>
2024-11-06 23:13:31 +0900

    feat: 자기소개 작성시 주는 보틀 삭제

--- app/src/main/kotlin/com/nexters/bottles/app/user/component/event/UserProfileApplicationEventListener.kt
+++ app/src/main/kotlin/com/nexters/bottles/app/user/component/event/UserProfileApplicationEventListener.kt
@@ -1,9 +1,9 @@
 package com.nexters.bottles.app.user.component.event
 
+import com.nexters.bottles.app.auth.event.SignUpEventDto
 import com.nexters.bottles.app.bottle.service.BottleHistoryService
 import com.nexters.bottles.app.bottle.service.BottleService
 import com.nexters.bottles.app.common.component.FileService
-import com.nexters.bottles.app.user.component.event.dto.IntroductionSaveEventDto
 import com.nexters.bottles.app.user.component.event.dto.UploadImageEventDto
 import com.nexters.bottles.app.user.service.UserService
 import org.springframework.scheduling.annotation.Async
@@ -20,7 +20,7 @@ class UserProfileApplicationEventListener(
 
     @Async
     @TransactionalEventListener
-    fun handleCustomEvent(event: IntroductionSaveEventDto) {
+    fun handleCustomEvent(event: SignUpEventDto) {
         var savedBottles = bottleService.matchFirstRandomBottle(event.userId, 3)
         savedBottles.forEach {
             bottleHistoryService.saveMatchingHistory(sourceUserId = it.sourceUser.id, targetUserId = it.targetUser.id)
--- app/src/main/kotlin/com/nexters/bottles/app/user/component/event/dto/IntroductionSaveEventDto.kt
+++
@@ -1,6 +0,0 @@
-package com.nexters.bottles.app.user.component.event.dto
-
-data class IntroductionSaveEventDto(
-    val userId: Long,
-) {
-}
